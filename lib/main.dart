d31fb415cfbfb0574edc91d5ba5c332465197137
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2021-11-02 13:15:13 +0330

    add end text for quiz

--- lib/main.dart
+++ lib/main.dart
@@ -16,48 +16,66 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'What\'s your favorite instructor?',
+      'answers': ['Max', 'Max', 'Max', 'Max'],
+    }
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion(int id) {
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      setState(() {
+        _questionIndex = _questionIndex + 1;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'What\'s your favorite instructor?',
-        'answers': ['Max', 'Max', 'Max', 'Max'],
-      }
-    ];
-
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'hello flutter',
-        ),
-      ),
-      body: Column(
-        children: <Widget>[
-          Question(questions[_questionIndex]['questionText'] as String),
-          ...(questions[_questionIndex]['answers'] as List<String>)
-              .map((answer) {
-            return Answer(_answerQuestion, answer);
-          }).toList(),
-        ],
-      ),
-    ));
+            appBar: AppBar(
+              title: Text(
+                'hello flutter',
+              ),
+            ),
+            body: _questionIndex < 3
+                ? Column(
+                    children: <Widget>[
+                      Question(
+                          questions[_questionIndex]['questionText'] as String),
+                      ...(questions[_questionIndex]['answers'] as List<String>)
+                          .map((answer) {
+                        return Answer(_answerQuestion, answer);
+                      }).toList(),
+                    ],
+                  )
+                : Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(top: 36),
+                    child: Column(
+                      children: [
+                        Text('you did it'),
+                        RaisedButton(
+                          onPressed: () => setState(() {
+                            _questionIndex = 0;
+                          }),
+                          child: Text('Try again'),
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                        )
+                      ],
+                    ),
+                  )));
   }
 }
